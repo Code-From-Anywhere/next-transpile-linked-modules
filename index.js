dadf14e7c001dcf0a814bcd0353913928e5733d9
Wijnand Karsens <wijnandworld@hotmail.com>
2022-01-03 11:44:46 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,35 @@
+const withLinksCreator = (linkableModules) => (nextConfig) => {
+  return Object.assign({}, nextConfig, {
+    webpack(config, options) {
+      if (options.isServer) {
+        config.externals = ["react", ...config.externals];
+      }
+
+      if (process.env.NODE_ENV === "development") {
+        config.watchOptions = {
+          poll: 2500,
+        };
+      }
+
+      config.resolve.alias = {
+        ...config.resolve.alias,
+        ...[...linkableModules, "react", "react-dom"].reduce(
+          (previous, module) => {
+            return {
+              ...previous,
+              [module]: path.resolve(__dirname, ".", "node_modules", module),
+            };
+          },
+          {}
+        ),
+      };
+
+      if (typeof nextConfig.webpack === "function") {
+        config = nextConfig.webpack(config, options);
+      }
+
+      console.log({ config: config.resolve.alias });
+      return config;
+    },
+  });
+};
