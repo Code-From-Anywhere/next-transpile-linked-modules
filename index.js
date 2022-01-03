c833600ae2cfe66446eea055df56830e715f1360
Wijnand Karsens <wijnandworld@hotmail.com>
2022-01-03 11:59:40 +0530

    Improvements

--- index.js
+++ index.js
@@ -3,30 +3,28 @@ const path = require("path");
 const withLinksCreator = (linkableModules) => (nextConfig) => {
   return Object.assign({}, nextConfig, {
     webpack(config, options) {
-      if (process.env.NEED_TRANSPILE_LINK_MODULES) {
-        if (options.isServer) {
-          config.externals = ["react", ...config.externals];
-        }
+      if (options.isServer) {
+        config.externals = ["react", ...config.externals];
+      }
 
-        if (process.env.NODE_ENV === "development") {
-          config.watchOptions = {
-            poll: 2500,
-          };
-        }
-
-        config.resolve.alias = {
-          ...config.resolve.alias,
-          ...[...linkableModules, "react", "react-dom"].reduce(
-            (previous, module) => {
-              return {
-                ...previous,
-                [module]: path.resolve(__dirname, ".", "node_modules", module),
-              };
-            },
-            {}
-          ),
+      if (process.env.NODE_ENV === "development") {
+        config.watchOptions = {
+          poll: 2500,
         };
       }
+
+      config.resolve.alias = {
+        ...config.resolve.alias,
+        ...[...linkableModules, "react", "react-dom"].reduce(
+          (previous, module) => {
+            return {
+              ...previous,
+              [module]: path.resolve(__dirname, ".", "node_modules", module),
+            };
+          },
+          {}
+        ),
+      };
 
       if (typeof nextConfig.webpack === "function") {
         config = nextConfig.webpack(config, options);
