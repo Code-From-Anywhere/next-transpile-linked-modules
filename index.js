b70b0589506af46d88fbd622c21702d491621d4c
Wijnand Karsens <wijnandworld@hotmail.com>
2022-01-03 11:51:45 +0530

    Improvements

--- index.js
+++ index.js
@@ -1,34 +1,35 @@
 const withLinksCreator = (linkableModules) => (nextConfig) => {
   return Object.assign({}, nextConfig, {
     webpack(config, options) {
-      if (options.isServer) {
-        config.externals = ["react", ...config.externals];
-      }
+      if (process.env.NEED_TRANSPILE_LINK_MODULES) {
+        if (options.isServer) {
+          config.externals = ["react", ...config.externals];
+        }
 
-      if (process.env.NODE_ENV === "development") {
-        config.watchOptions = {
-          poll: 2500,
+        if (process.env.NODE_ENV === "development") {
+          config.watchOptions = {
+            poll: 2500,
+          };
+        }
+
+        config.resolve.alias = {
+          ...config.resolve.alias,
+          ...[...linkableModules, "react", "react-dom"].reduce(
+            (previous, module) => {
+              return {
+                ...previous,
+                [module]: path.resolve(__dirname, ".", "node_modules", module),
+              };
+            },
+            {}
+          ),
         };
       }
-
-      config.resolve.alias = {
-        ...config.resolve.alias,
-        ...[...linkableModules, "react", "react-dom"].reduce(
-          (previous, module) => {
-            return {
-              ...previous,
-              [module]: path.resolve(__dirname, ".", "node_modules", module),
-            };
-          },
-          {}
-        ),
-      };
 
       if (typeof nextConfig.webpack === "function") {
         config = nextConfig.webpack(config, options);
       }
 
-      console.log({ config: config.resolve.alias });
       return config;
     },
   });
